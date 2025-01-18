34ae589e01ac3b060e652cc618e75915e5f46c23
Sankalptripathi231217 <sankalptripathi3575@gmail.com>
2025-01-19 02:17:01 +0530

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    int a=4, b=5, c=6;
+
+    int sum=a+b+c;
+    printf("Sum is: %d",sum);
+}
---
+++ sumOfTwoNo.c
@@ -0,0 +1,19 @@
+// sum of two no withot input function 
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a=2,b=3,sum;
+    sum=a+b;
+    printf("sum of %d and %d = %d",a,b,sum);
+    
+    getch();
+
+}
+
+
+
+
+
+
+
